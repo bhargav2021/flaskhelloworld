eb012a55d8c40a4b55a7b07c1b390460d51715f4
bhargav2021 <34270006+bhargav2021@users.noreply.github.com>
2018-01-25 11:52:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
-
 import joblib
 import numpy
 from flask import Flask, render_template,request
 import pandas
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
   return render_template("values.html")
 
 
-@app.route('/predict')
+@app.route('/predict', methods=['POST'])
 def predict():
    clf  = joblib.load("two_values_model.pkl")
    m =clf.predict([[value , 2nd value]])
    return m
-
-
-
 
 if __name__ == '__main__':
   app.run()
