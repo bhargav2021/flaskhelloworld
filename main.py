fa46326566561e971b0cf87214fbfef582c8425f
bhargav2021 <34270006+bhargav2021@users.noreply.github.com>
2018-01-25 11:39:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 
 import boost
 import numpy
-from flask import Flask
+from flask import Flask, render_template,request
 import pandas
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hey its Python Flask application Success!'
+  return render_template(values.html)
+
+
+@app.route('/predict')
+def predict():
+   clf  = joblib.load(models_dir + model + "/" + model + ".pkl")
+   m =clf.predict([[1st value , 2nd value]])
+   return m
+
+
+
 
 if __name__ == '__main__':
   app.run()
